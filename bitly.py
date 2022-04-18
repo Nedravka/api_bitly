38ae2f4a771e609355a2128da14a535d4d38ca2f
SZakalyuzniy <zakalsergey@mail.ru>
2022-04-18 22:10:22 +0400

    add code

--- bitly.py
+++ bitly.py
@@ -2,49 +2,62 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
-def shorten_links(TOKEN, link_api_bitly, url):
+LINK_API_BITLY = 'https://api-ssl.bitly.com/v4/'
+load_dotenv()
+TOKEN = os.getenv("BITLY_TOKEN")
+HEADERS = {
+    'Authorization': f'Bearer {TOKEN}'
+}
+
+
+def shorten_links(url, headers):
     url_for_shorten = {
         'long_url': f'{url}'
     }
-
-    short_link = requests.post(f'{link_api_bitly}shorten', headers=headers, json=url_for_shorten)
+    short_link = requests.post(f'{LINK_API_BITLY}shorten', headers=headers, json=url_for_shorten)
     short_link.raise_for_status()
-
     return short_link.json()['id']
 
 
-def count_clicks(TOKEN, link_api_bitly, bitlink):
-    response_number_of_clicks_on_bitlink = requests.get(f'{link_api_bitly}bitlinks/{bitlink}/clicks/summary', headers=headers)
+def count_clicks(bitlink, headers):
+    parse_url = urlparse(bitlink)
+    response_number_of_clicks_on_bitlink = requests.get(f'{LINK_API_BITLY}bitlinks/{parse_url.netloc}{parse_url.path}/clicks/summary', headers=headers)
     response_number_of_clicks_on_bitlink.raise_for_status()
-
     return response_number_of_clicks_on_bitlink.json()['total_clicks']
 
 
-def is_bitlink(url):
-    check_url = requests.get(f'{link_api_bitly}bitlinks/{url}', headers=headers)
-    if check_url.ok:
-        return True
-    return False
+def is_bitlink(url, headers):
+    parse_url = urlparse(url)
+    check_url = requests.get(f'{LINK_API_BITLY}bitlinks/{parse_url.netloc}{parse_url.path}', headers=headers)
+    return check_url.ok
+
+
+def check_url_accessibility(url):
+    return requests.get(url).raise_for_status()
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    TOKEN = os.getenv("BITLY_TOKEN")
-    link_api_bitly = 'https://api-ssl.bitly.com/v4/'
-    url = input('Введи ссылку для сокращения: ') #'https://www.atlassian.com/ru/git/tutorials/saving-changes/gitignore' 'bit.ly/3jx2kO8'
-    headers = {
-        'Authorization': f'Bearer {TOKEN}'
-    }
-    if is_bitlink(url):
-        print('Количество кликов: ', count_clicks(TOKEN, link_api_bitly, url))
-    else:
-        try:
-            validate_url = requests.get(url).raise_for_status()
-            print('Битлинк: ', shorten_links(TOKEN, link_api_bitly, url))
-        except:# requests.exceptions.HTTPError as error:
-            # if error.response.status_code == 400:
+
+    url = input('Введи ссылку для сокращения: ')
+    try:
+        check_url_accessibility(url)
+        if is_bitlink(url, HEADERS):
+            print('Количество кликов: ', count_clicks(url, HEADERS))
+        else:
+            print('Битлинк: ', shorten_links(url, HEADERS))
+    except requests.exceptions.HTTPError as err:
+        print(err.response.status_code)
+        if err.response.status_code in [400, 404]:
             print('Не корректная ссылка')
-            # else:
-            #     raise requests.exceptions.HTTPError
+        elif err.response.status_code == 403:
+            print('Проверьте токен доступа')
+        else:
+            raise
+    except requests.exceptions.MissingSchema:
+        print('Веедите ссылку в формате https://...')
+
+
+
